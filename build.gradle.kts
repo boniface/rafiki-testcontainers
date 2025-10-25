494984404a4e6fe92c6b79b5654a2d6061691aee
boniface <550236+boniface@users.noreply.github.com>
2025-10-25 11:40:26 -0400

    Prepare for v0.1.0 release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
     java
-    `maven-publish`
-    signing
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("com.vanniktech.maven.publish") version "0.30.0"
     id("io.github.sgtsilvio.gradle.metadata") version "0.6.0"
 }
 
@@ -67,55 +65,36 @@ metadata {
     }
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            pom {
-                name.set("Rafiki Testcontainers")
-                description.set("Testcontainers wrapper for Rafiki - Interledger open payments backend for integration testing")
-                url.set("https://github.com/hashcode-zm/rafiki-testcontainers")
-                licenses {
-                    license {
-                        name.set("Apache-2.0")
-                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
-                    }
-                }
-                scm {
-                    url.set("https://github.com/hashcode-zm/rafiki-testcontainers")
-                    connection.set("scm:git:git://github.com/hashcode-zm/rafiki-testcontainers.git")
-                    developerConnection.set("scm:git:ssh://github.com:hashcode-zm/rafiki-testcontainers.git")
-                }
-                developers {
-                    developer {
-                        id.set("boniface")
-                        name.set("Boniface Kabaso")
-                        email.set("550236+boniface@users.noreply.github.com")
-                    }
-                }
+mavenPublishing {
+    publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    coordinates("zm.hashcode", "rafiki-testcontainers", "0.1.0")
+
+    pom {
+        name.set("Rafiki Testcontainers")
+        description.set("Testcontainers wrapper for Rafiki - Interledger open payments backend for integration testing")
+        url.set("https://github.com/hashcode-zm/rafiki-testcontainers")
+
+        licenses {
+            license {
+                name.set("Apache-2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0")
             }
         }
-    }
-}
 
-signing {
-    val signingKeyId: String? = project.findProperty("signingKeyId") as String? ?: System.getenv("SIGNING_KEY_ID")
-    val signingKey: String? = project.findProperty("gpgPrivateKey") as String? ?: System.getenv("GPG_PRIVATE_KEY")
-    val signingPassword: String? = project.findProperty("gpgPassphrase") as String? ?: System.getenv("GPG_PASSPHRASE")
+        developers {
+            developer {
+                id.set("boniface")
+                name.set("Boniface Kabaso")
+                email.set("550236+boniface@users.noreply.github.com")
+            }
+        }
 
-    if (signingKey != null && signingPassword != null) {
-        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
-        sign(publishing.publications)
-    }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(project.findProperty("centralPortalUsername") as String? ?: System.getenv("CENTRAL_PORTAL_USERNAME"))
-            password.set(project.findProperty("centralPortalPassword") as String? ?: System.getenv("CENTRAL_PORTAL_PASSWORD"))
+        scm {
+            url.set("https://github.com/hashcode-zm/rafiki-testcontainers")
+            connection.set("scm:git:git://github.com/hashcode-zm/rafiki-testcontainers.git")
+            developerConnection.set("scm:git:ssh://git@github.com:hashcode-zm/rafiki-testcontainers.git")
         }
     }
 }
